63ed654776bcaa3ac313f49407393e32c437ae54
parksomin72 <fatimazahraealawi@gmail.com>
2023-11-28 12:58:26 +0000

    Binary trees files

--- binary_trees.h
+++ binary_trees.h
@@ -37,7 +37,53 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* Task 3 */
 void binary_tree_delete(binary_tree_t *tree);
 
+/* Task 4 */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 9 */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* Task 10 */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* Task 11 */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/* Task 12 */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/* Task 13 */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/* Task 14 */
+int binary_tree_balance(const binary_tree_t *tree);
+
+/* Task 15 */
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/* Task 16 */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* Task 17 */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* Task 18 */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 /* Function to Print Binary Tree (for visualization) */
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
+

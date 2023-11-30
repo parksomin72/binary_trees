d067c7156c87636ca033e8882bcb697bf7c4c368
parksomin72 <fatimazahraealawi@gmail.com>
2023-11-30 11:53:53 +0000

    Binary Trees files

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -8,26 +8,26 @@
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+if (tree == NULL)
+return;
 
-    printf("%d\n", tree->n);
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        printf("Left child: ");
-        if (tree->left != NULL)
-            printf("%d", tree->left->n);
-        else
-            printf("NULL");
+printf("%d\n", tree->n);
+if (tree->left != NULL || tree->right != NULL)
+{
+printf("Left child: ");
+if (tree->left != NULL)
+printf("%d", tree->left->n);
+else
+printf("NULL");
 
-        printf("\nRight child: ");
-        if (tree->right != NULL)
-            printf("%d", tree->right->n);
-        else
-            printf("NULL");
-        printf("\n");
-    }
+printf("\nRight child: ");
+if (tree->right != NULL)
+printf("%d", tree->right->n);
+else
+printf("NULL");
+printf("\n");
+}
 
-    binary_tree_print(tree->left);
-    binary_tree_print(tree->right);
+binary_tree_print(tree->left);
+binary_tree_print(tree->right);
 }

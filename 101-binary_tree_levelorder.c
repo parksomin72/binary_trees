6dd3838b8fbdfb13811c90f2cbada37e4fee77b2
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-01 22:09:15 +0000

    new

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,31 +2,31 @@
 #include "queues.h"
 #include "queues.c"
 /**
- * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * binary_tree_levelorder _Goes through a binary tree using level-order
  *
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    queue_t *queue = NULL;
-    const binary_tree_t *current;
+queue_t *queue = NULL;
+const binary_tree_t *current;
 
-    if (tree == NULL || func == NULL)
-        return;
+if (tree == NULL || func == NULL)
+return;
 
-    enqueue(&queue, (binary_tree_t *)tree);
+enqueue(&queue, (binary_tree_t *)tree);
 
-    while (queue != NULL)
-    {
-        current = dequeue(&queue);
+while (queue != NULL)
+{
+current = dequeue(&queue);
 
-        func(current->n);
+func(current->n);
 
-        if (current->left != NULL)
-            enqueue(&queue, current->left);
+if (current->left != NULL)
+enqueue(&queue, current->left);
 
-        if (current->right != NULL)
-            enqueue(&queue, current->right);
-    }
+if (current->right != NULL)
+enqueue(&queue, current->right);
+}
 }

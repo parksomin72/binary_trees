93f41e6b1dbd7f0c2e092115e1323d3e5ed3ad13
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-01 23:20:15 +0000

    new

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include "queues.h"
+#include "queues.c"
 
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete
@@ -9,33 +10,31 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    queue_t *front = NULL;
-    int flag = 0; 
+queue_t *front = NULL;
+int flag = 0;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    /* Use level-order traversal to check completeness */
 
-    enqueue(&front, (binary_tree_t *)tree);
+enqueue(&front, (binary_tree_t *)tree);
 
-    while (front != NULL)
-    {
-        binary_tree_t *current = dequeue(&front);
+while (front != NULL)
+{
+binary_tree_t *current = dequeue(&front);
 
-        /* Check if a non-full node is encountered */
-        if (current == NULL)
-            flag = 1;
-        else
-        {
-            /* If a non-full node was encountered before, and a non-NULL node is found, the tree is not complete */
-            if (flag)
-                return (0);
 
-            enqueue(&front, current->left);
-            enqueue(&front, current->right);
-        }
-    }
+if (current == NULL)
+flag = 1;
+else
+{
+if (flag)
+return (0);
 
-    return (1);
+enqueue(&front, current->left);
+enqueue(&front, current->right);
+}
+}
+
+return (1);
 }

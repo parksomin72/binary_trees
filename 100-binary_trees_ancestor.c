492ef35ac4dd041199c99af44f45a0dd76363f51
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-01 01:13:37 +0000

    new

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,27 +8,28 @@
  *
  * Return: Pointer to the lowest common ancestor node, or NULL if no ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
 
-    const binary_tree_t *temp1 = first;
-    const binary_tree_t *temp2;
-    if (first == NULL || second == NULL)
-        return NULL;
+const binary_tree_t *temp1 = first;
+const binary_tree_t *temp2;
+if (first == NULL || second == NULL)
+return NULL;
 
-    while (temp1)
-    {
-        temp2 = second;
+while (temp1)
+{
+temp2 = second;
 
-        while (temp2)
-        {
-            if (temp1 == temp2)
-                return (binary_tree_t *)temp1;
-            temp2 = temp2->parent;
-        }
+while (temp2)
+{
+if (temp1 == temp2)
+return (binary_tree_t *)temp1;
+temp2 = temp2->parent;
+}
 
-        temp1 = temp1->parent;
-    }
+temp1 = temp1->parent;
+}
 
-    return NULL;
+return NULL;
 }

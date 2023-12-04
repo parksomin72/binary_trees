e37c957b67e7ca63f6d5c60763812279162f5b32
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-04 09:54:56 +0000

    new

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,39 +2,55 @@
 #include "queues.h"
 #include "queues.c"
 
+#include "binary_trees.h"
+
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure.
  *
- * Return: 1 if the tree is complete, 0 otherwise
+ * Return: The size of the tree. If tree is NULL, return 0.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+}
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is complete, 0 otherwise.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-queue_t *front = NULL;
-int flag = 0;
+    size_t index = 0;
+    size_t size = binary_tree_size(tree);
 
-if (tree == NULL)
-return (0);
+    if (tree == NULL)
+        return 0;
 
-
-enqueue(&front, (binary_tree_t *)tree);
-
-while (front != NULL)
-{
-binary_tree_t *current = dequeue(&front);
-
-
-if (current == NULL)
-flag = 1;
-else
-{
-if (flag)
-return (0);
-
-enqueue(&front, current->left);
-enqueue(&front, current->right);
-}
+    return is_complete(tree, index, size);
 }
 
-return (1);
+/**
+ * is_complete - Checks if a binary tree is complete recursively.
+ * @tree: A pointer to the root node of the tree to check.
+ * @index: The index of the current node.
+ * @size: The size of the entire tree.
+ *
+ * Return: 1 if the tree is complete, 0 otherwise.
+ */
+int is_complete(const binary_tree_t *tree, size_t index, size_t size)
+{
+    if (tree == NULL)
+        return 1;
+
+    if (index >= size)
+        return 0;
+
+    return (is_complete(tree->left, 2 * index + 1, size) &&
+            is_complete(tree->right, 2 * index + 2, size));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -71,6 +71,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+int is_complete(const binary_tree_t *tree, size_t index, size_t size);
 
 #endif /* BINARY_TREES_H */
 

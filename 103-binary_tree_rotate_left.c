52b1baf9401663a8e921cd2bdb48ad732f94e4a2
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-04 12:04:44 +0000

    new

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,23 +1,19 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree, 0 if tree is NULL.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+    binary_tree_t *new_root = tree->right;
 
-    if (tree == NULL)
-        return (0);
+    if (new_root == NULL)
+        return tree;
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    tree->right = new_root->left;
+    if (new_root->left != NULL)
+        new_root->left->parent = tree;
 
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+    new_root->left = tree;
+    new_root->parent = tree->parent;
+    tree->parent = new_root;
+
+    return new_root;
 }
--- binary_trees.h
+++ binary_trees.h
@@ -74,5 +74,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int is_complete(const binary_tree_t *tree, size_t index, size_t size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
+
 #endif /* BINARY_TREES_H */
 

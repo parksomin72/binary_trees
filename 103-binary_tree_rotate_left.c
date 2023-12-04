3f960760f435ab869b09e7f28bbb8326b692c4c9
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-04 10:18:51 +0000

    new

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,31 +9,30 @@
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 binary_tree_t *new_root = tree->right;
-    tree->right = new_root->left;
-    if (tree == NULL || tree->right == NULL)
-        return (tree);
+tree->right = new_root->left;
+if (tree == NULL || tree->right == NULL)
+return (tree);
 
+if (new_root->left != NULL)
+new_root->left->parent = tree;
 
-    if (new_root->left != NULL)
-        new_root->left->parent = tree;
+new_root->parent = tree->parent;
 
-    new_root->parent = tree->parent;
+if (tree->parent == NULL)
+{
+tree->parent = new_root;
+new_root->parent = NULL;
+}
+else
+{
+if (tree->parent->left == tree)
+tree->parent->left = new_root;
+else
+tree->parent->right = new_root;
+}
 
-    if (tree->parent == NULL)
-    {
-        tree->parent = new_root;
-        new_root->parent = NULL;
-    }
-    else
-    {
-        if (tree->parent->left == tree)
-            tree->parent->left = new_root;
-        else
-            tree->parent->right = new_root;
-    }
+new_root->left = tree;
+tree->parent = new_root;
 
-    new_root->left = tree;
-    tree->parent = new_root;
-
-    return (new_root);
+return (new_root);
 }
